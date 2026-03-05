5211cdda6ff55cb0fa0b3d785af3a0b0c6d18a6c
MUBASHIR <mubashirpp695@gmail.com>
2026-03-05 20:01:11 +0530

    add robots.txt

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+        rules: {
+            userAgent: "*",
+            allow: "/",
+        },
+        sitemap: "https://orvynlabs.in/sitemap.xml",
+    };
+}
